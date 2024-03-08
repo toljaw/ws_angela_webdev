a940d77dcdde616a3ecee0ad29aa0dcc537ec858
Anatoli Weikum <weikum.anatoli@gmail.com>
2024-03-08 18:07:16 +0100

    add js exercise 14.0 and 14.1

---
+++ 14.0 Introduction to Javascript ES6/01_hello_world.js
@@ -0,0 +1,5 @@
+console.log("Hello World");
+console.log(3+2);
+var name = "Anatoli"
+console.log(`My name is ${name}.`)
+alert("Hallo, Welt!");
---
+++ 14.1 input_output/index.js
@@ -0,0 +1,3 @@
+alert("Hey my name is Anatoli.");
+var your_name = prompt("Whats your name? ");
+alert(`Hi ${your_name}, nice to meet you!`);
