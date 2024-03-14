c0821cfd5bc1a3b7cee461123197f9eca378ab9a
Anatoli Weikum <weikum.anatoli@gmail.com>
2024-03-14 17:55:05 +0100

    create project Simon Game 20.0, add function for game sequence, add animations and sounds for user

---
+++ 01 Frontend Chapter 1 to 20/20.0 Simon Game/game.js
@@ -0,0 +1,13 @@
+const buttonColours = ["red", "blue", "green", "yellow"];
+const gamePattern = [];
+
+function nextSequence() {
+    let randomNumber = Math.floor(Math.random()*4);
+    let randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    
+    let audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();
+}
